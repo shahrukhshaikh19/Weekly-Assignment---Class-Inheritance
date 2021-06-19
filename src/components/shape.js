a4f836feaa118e22e3d5ba8dce15f0f7e5b68927
shahrukhshaikh19 <79331339+shahrukhshaikh19@users.noreply.github.com>
2021-06-19 16:01:41 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
 
 // export class using module.exports
+import React from "react";
+
+class Shape {
+  constructor() {
+    this.color = "#fffff";
+  }
+  drawShape() {
+    console.log("draw");
+  }
+  calculateArea() {
+    console.log("area");
+  }
+  render() {
+    return <h1>Shape</h1>;
+  }
+}
+module.exports = Shape;
