95ad0b68a935d76a6db09bd6f90d052ccfccffa4
shahrukhshaikh19 <79331339+shahrukhshaikh19@users.noreply.github.com>
2021-06-19 16:00:34 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,18 @@
 
 // declare class
 
-
 // export class using module.exports
+
+let Shape = require("./shape");
+class Circle extends Shape {
+  constructor(props) {
+    super(props);
+  }
+  calculateArea() {
+    console.log("circle area");
+  }
+  render() {
+    return <h1>Circle</h1>;
+  }
+}
+module.exports = Circle;
